37554b47bbb05f7a1ac5767dba6687f0392ac068
Your Name <you@example.com>
2025-10-23 12:25:13 +0300

    implement game logic

--- game_logic/game.py
+++ game_logic/game.py
@@ -1,6 +1,44 @@
-def create_player(name:str) -> dict:
-    return {}
+from utils import deck
+
+def create_player(name: str ='AI') -> dict:
+    return {'name': name, 'hand': [], 'won_pile': []}
+
+
 def init_game() -> dict:
-    return {}
-def play_round(p1:dict,p2:dict):
-    return None
+    player_1 = create_player("Avi")
+    player_2 = create_player()
+    new_deck = deck.create_deck()
+    shuffled_deck = deck.shuffle(new_deck)
+
+    player_1_hand = shuffled_deck[27:]
+    player_2_hand = shuffled_deck[:27]
+
+    player_1['hand'] = player_1_hand
+    player_2['hand'] = player_2_hand
+
+    return {
+        'deck': shuffled_deck,
+        'player_1': player_1,
+        'player_2': player_2
+    }
+
+
+def play_round(p1: dict, p2: dict):
+    p1_hand = p1['hand']
+    p1_top_card = p1_hand[-1]
+
+    p2_hand = p2['hand']
+    p2_top_card = p2_hand[-1]
+
+    winer = deck.compare_cards(p1_top_card, p2_top_card)
+    if winer == 'p1':
+        won = p2['hand'].pop()
+        p1['won_pile'].append(won)
+        p1['won_pile'].append(p1_hand.pop())
+
+    if winer == 'p2':
+        won = p1['hand'].pop()
+        p2['won_pile'].append(won)
+        p2['won_pile'].append(p2_hand.pop())
+    else:
+        pass
